2fdc16ad986750619ccd0f5a9183bfa04b15d282
lucas correia <lfpgc96@hotmail.com>
2019-10-26 13:45:52 -0300

    Main Menu

---
+++ Voxeland/Assets/Scripts/LevelChanger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChanger : MonoBehaviour
+{
+    private string level;
+    public Animator fade;
+
+    public void FadeToLevel(string level)
+    {
+        this.level = level;
+        fade.SetTrigger("Fade");
+    }
+    public void OnFadeComplete()
+    {
+        SceneManager.LoadScene(level);
+    }
+}
